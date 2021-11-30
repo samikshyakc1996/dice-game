634cb6f6befb15446050dccc1eff439fbe9106d1
samikshyakc1996 <samikshyakc1996@gmail.com>
2021-11-30 11:23:23 -0500

    dice game completed with lots of console msgs

--- js/script.js
+++ js/script.js
@@ -1,18 +1,41 @@
 // This function returns a random Number between 1 (inclusive) and the argument passed to the function
 // If no argument is passed, 6 is set, ie, returns a number between and including 1 to 6
-// 		For example: 
+// 		For example:
 //    getRandomDiceRoll() will return either 1, 2, 3, 4, 5, 6
-const getRandomDiceRoll = function(sides=6) {
-  return Math.floor( Math.random() * sides ) + 1
-}
+const getRandomDiceRoll = function (sides = 6) {
+  return Math.floor(Math.random() * sides) + 1;
+};
+let lastRoll = 0;
+const rollTheDice = function () {
+  // Here's a test roll, check the console!
 
-// Here's a test roll, check the console!
-console.log(getRandomDiceRoll())
+  // 1) Call getRandomDiceRoll() and store the result as a variable named "diceRoll"
+  let diceRoll = getRandomDiceRoll();
+  console.log(diceRoll);
+  // 2) Update user interface (document), showing the diceface (svg image) that matches the roll number
 
-// 1) Call getRandomDiceRoll() and store the result as a variable named "diceRoll"
+  let eleMessage = document.querySelector(`#message`);
+  let rollStr = ["zero", "one", "two", "three", "four", "five", "six"];
+  if (diceRoll === lastRoll) {
+    console.log("same");
+    eleMessage.innerText = `You rolled: ${rollStr[diceRoll]} again`;
+  } else {
+    console.log("different");
+    eleMessage.innerText = `You rolled: ${rollStr[diceRoll]}`;
+  }
+  console.log(`Your Roll is ${diceRoll}`);
+  console.log(`Your last Rol is ${lastRoll}`);
+  lastRoll = diceRoll;
+  let eleDice = document.querySelector(`#dice`);
+  //set dice's src attribute
+  eleDice.setAttribute(`src`, `img/dice${diceRoll}.svg`);
 
-// 2) Update user interface (document), showing the diceface (svg image) that matches the roll number
+  //update dice's alt attribute
+  eleDice.setAttribute(`alt`, `Dice${diceRoll}`);
 
-// 3) Use `diceRoll` to update the label "You rolled: #" (replacing # with the roll)
+  // 3) Use `diceRoll` to update the label "You rolled: #" (replacing # with the roll)
 
-// 4) Wrap the dice roll procedure in a function named rollTheDice(), call it from the console to test
+  // 4) Wrap the dice roll procedure in a function named rollTheDice(), call it from the console to test
+};
+eleBtn = document.querySelector(`#btn`);
+eleBtn.addEventListener(`click`, rollTheDice);
