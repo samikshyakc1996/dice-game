cc286239e8e352f2ae7845846762770363dbd9d4
samikshyakc1996 <samikshyakc1996@gmail.com>
2021-12-02 03:18:20 -0500

    Two dices casino game done. implemented arrays and logics

--- js/script.js
+++ js/script.js
@@ -6,16 +6,54 @@ const getRandomDiceRoll = function (sides = 6) {
   return Math.floor(Math.random() * sides) + 1;
 };
 let lastRoll = 0;
+let lastMessage = "";
 const rollTheDice = function () {
   // Here's a test roll, check the console!
 
   // 1) Call getRandomDiceRoll() and store the result as a variable named "diceRoll"
   let diceRoll = getRandomDiceRoll();
+  let diceRollII = getRandomDiceRoll();
   console.log(diceRoll);
+  console.log(diceRollII);
+  let sumValue = diceRoll + diceRollII;
   // 2) Update user interface (document), showing the diceface (svg image) that matches the roll number
 
   let eleMessage = document.querySelector(`#message`);
-  let rollStr = ["zero", "one", "two", "three", "four", "five", "six"];
+  let rollStr = ["zero", "one", "four", "six", "eight", "ten"];
+
+  let oddRollStr = [
+    "Zero",
+    "Ace Duece",
+    "Five(Fever Five)",
+    "Natural or Seven Out",
+    "Nine(Nina)",
+    "Yo(Yo-leven)",
+  ];
+
+  if (diceRoll === 1 && diceRollII === 1) {
+    console.log("snake Eyes");
+    eleMessage.innerText = `You Rolled Snake Eyes`;
+  } else if (diceRoll === 6 && diceRollII === 6) {
+    console.log(
+      `${diceRollII} ${diceRoll} = 6 so you rolled Boxcars or Midnight`
+    );
+    eleMessage.innerText = `You Rolled Boxcars or Midnight`;
+  } else if (sumValue % 2 == 1) {
+    sumValue = Math.floor(sumValue / 2);
+
+    console.log(`${oddRollStr[sumValue]}`);
+    eleMessage.innerText = `You Rolled: ${oddRollStr[sumValue]}`;
+  } else if (diceRoll === diceRollII) {
+    eleMessage.innerText = `You rolled hard ${rollStr[sumValue / 2]}`;
+    console.log(`${diceRoll} + ${diceRollII} =hard rolled`);
+  } else if (diceRoll !== diceRollII && sumValue % 2 === 0 && sumValue !== 2) {
+    console.log(`${rollStr[sumValue / 2]}`);
+    eleMessage.innerText = `You rolled easy ${rollStr[sumValue / 2]}`;
+    console.log("easy rolled");
+  } else {
+    console.log("I am not sure");
+  }
+  /*  // FOR ONE DICE
   if (diceRoll === lastRoll) {
     console.log("same");
     eleMessage.innerText = `You rolled: ${rollStr[diceRoll]} again`;
@@ -26,12 +64,29 @@ const rollTheDice = function () {
   console.log(`Your Roll is ${diceRoll}`);
   console.log(`Your last Rol is ${lastRoll}`);
   lastRoll = diceRoll;
+
+  */
+
+  if (eleMessage.innerText === lastMessage) {
+    eleMessage.innerText = `${lastMessage} again`;
+    console.log(`${lastMessage} again!`);
+  }
+  lastMessage = eleMessage.innerText;
+
   let eleDice = document.querySelector(`#dice`);
   //set dice's src attribute
   eleDice.setAttribute(`src`, `img/dice${diceRoll}.svg`);
 
   //update dice's alt attribute
   eleDice.setAttribute(`alt`, `Dice${diceRoll}`);
+
+  // diceII
+  let eleDiceII = document.querySelector(`#diceII`);
+  //set diceII's src attribute
+  eleDiceII.setAttribute(`src`, `img/dice${diceRollII}.svg`);
+
+  //update dice's alt attribute
+  eleDiceII.setAttribute(`alt`, `Dice${diceRollII}`);
 
   // 3) Use `diceRoll` to update the label "You rolled: #" (replacing # with the roll)
 
